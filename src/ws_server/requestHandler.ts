37dda218dbcd6e837d5568b0a16a1f8986c61847
firespark <fireleit@gmail.com>
2024-10-23 19:29:27 +0400

    feat: add start_game function

--- src/ws_server/requestHandler.ts
+++ src/ws_server/requestHandler.ts
@@ -9,19 +9,29 @@ const gameHistory: Game[] = [];
 export class Game {
     idGame: number | string;
     idPlayer: number | string;
-    constructor(idPlayer: number | string)
+    ships: Ship[];
+    constructor(gameIndex: number | string, idPlayer: number | string)
     {
-        this.idGame = gameHistory.length;
+        this.idGame = gameIndex;
         this.idPlayer = idPlayer;
+        this.ships = [];
     }
 }
 
-export function create_game(roomId: number){
+export type Ship = {
+    position: {x:number, y:number};
+    direction: boolean;
+    type: string;
+    length: number;
+}
 
+export function create_game(roomId: number){
+    const gameIndex = gameHistory.length;
     rooms[roomId].roomUsers.forEach(player => {
         const socket = activeSockets.get(player.index)
         if (socket != undefined){
-            const currentGame = new Game(player.index);
+            const currentGame = new Game(gameIndex, player.index);
+            gameHistory.push(currentGame);
             let response: types.reqOutputInt = new types.Reponse('create_game', JSON.stringify(currentGame));
             socket.send(JSON.stringify(response))
         }
@@ -65,9 +75,30 @@ export const requestHandler = (req: types.reqInputInt, socket: WebSocket) => {
             {
                 create_game(data.indexRoom)
             }
-           
             break;
 
+        case 'add_ships':
+            let playerReadyCount = 0;
+            let sessions = gameHistory.filter((game) => {
+               return game.idGame == data.gameId;
+            });
+            sessions.forEach(game => {
+                if (game.idPlayer == data.indexPlayer) {
+                    game.ships = data.ships;
+                }
+                console.log(game.ships)
+                if (game.ships.length > 0)
+                    playerReadyCount++;
+            });
+            if (playerReadyCount == 2)
+                sessions.forEach((session) => {
+                    const socketPlayer = activeSockets.get(session.idPlayer);
+                    if (socketPlayer != undefined){
+                    const currentGame = {ships: session.ships, currentPlayerIndex: session.idPlayer};
+                    let response: types.reqOutputInt = new types.Reponse('start_game', JSON.stringify(currentGame));
+                    socketPlayer.send(JSON.stringify(response))}
+                })
+            break;
         
 
         default:
