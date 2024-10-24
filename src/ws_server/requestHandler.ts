a457cbdcb99d7f941de2abde645d5b2bfdd85e22
firespark <fireleit@gmail.com>
2024-10-24 20:32:42 +0400

    feat: add wipe of surrounding cells on kill

--- src/ws_server/requestHandler.ts
+++ src/ws_server/requestHandler.ts
@@ -32,7 +32,7 @@ export class RunningGame {
 
         this.damagedCells.push(damagedCellsP1);
         this.damagedCells.push(damagedCellsP2);
-        
+
         let shipsP1: GameShip[] = [];
         let shipsP2: GameShip[] = [];
         sessions[0].ships.forEach((ship) => {
@@ -46,7 +46,7 @@ export class RunningGame {
         this.ships.push(shipsP2);
     }
 
-    attack(coordinate: types.coordinate, attackerID: number | string):attackReport {
+    attack(coordinate: types.coordinate, attackerID: number | string): attackReport {
         let attackerIndex = this.players.indexOf(attackerID);
         let victimIndex = attackerIndex ? 0 : 1;
         this.damagedCells[victimIndex].push(coordinate);
@@ -55,11 +55,11 @@ export class RunningGame {
             currentPlayer: attackerID,
             status: 'miss'
         }
-        for (let i = 0; i < this.ships[victimIndex].length; i++){
-            
+        for (let i = 0; i < this.ships[victimIndex].length; i++) {
+
             const victimDamageReport = this.ships[victimIndex][i].damageCheck(coordinate)
             if (victimDamageReport != 'miss') {
-             console.log(victimDamageReport)
+                console.log(victimDamageReport)
                 damageReport.status = victimDamageReport;
             }
         }
@@ -69,15 +69,15 @@ export class RunningGame {
 
     }
 
-    isValidShot(coordinate: types.coordinate, attackerID: number | string):boolean {
+    isValidShot(coordinate: types.coordinate, attackerID: number | string): boolean {
         let attackerIndex = this.players.indexOf(attackerID);
         let victimIndex = attackerIndex ? 0 : 1;
 
         for (let i = 0; i < this.damagedCells[victimIndex].length; i++) {
-            if (this.damagedCells[victimIndex][i].x == coordinate.x && this.damagedCells[victimIndex][i].y == coordinate.y){
+            if (this.damagedCells[victimIndex][i].x == coordinate.x && this.damagedCells[victimIndex][i].y == coordinate.y) {
                 return false;
             }
-            
+
         }
         return true;
     }
@@ -88,19 +88,19 @@ export class RunningGame {
 
         const allCoordinates: types.coordinate[] = [];
         for (let x = 0; x < 10; x++) {
-          for (let y = 0; y < 10; y++) {
-            allCoordinates.push({ x, y });
-          }
+            for (let y = 0; y < 10; y++) {
+                allCoordinates.push({ x, y });
+            }
         }
         const availableCoordinates = allCoordinates.filter(
-          coord => !coordinateExists(this.damagedCells[victimIndex], coord)
+            coord => !coordinateExists(this.damagedCells[victimIndex], coord)
         );
         if (availableCoordinates.length === 0) {
-            return {x:666,y:666}
+            return { x: 666, y: 666 }
         }
         const randomIndex = Math.floor(Math.random() * availableCoordinates.length);
         return availableCoordinates[randomIndex];
-      }
+    }
 }
 
 export type attackReport = {
@@ -121,15 +121,15 @@ export class GameShip {
         this.maxHP = ship.length;
         this.status = 'healthy';
         for (let i = 0; i < ship.length; i++) {
-            let coordinate = {x: ship.position.x, y: ship.position.y};
-            
+            let coordinate = { x: ship.position.x, y: ship.position.y };
+
             if (ship.direction) {
                 coordinate.y += i;
             }
             else {
                 coordinate.x += i;
             }
-            
+
             this.cells.push(coordinate);
         }
     }
@@ -137,10 +137,10 @@ export class GameShip {
     damageCheck(cell: types.coordinate): string {
         let hitReport = 'miss';
         for (let i = 0; i < this.cells.length; i++) {
-            if (this.cells[i].x == cell.x && this.cells[i].y == cell.y){
+            if (this.cells[i].x == cell.x && this.cells[i].y == cell.y) {
                 console.log(this.cells)
                 this.hp--;
-                if (this.hp <= 0){
+                if (this.hp <= 0) {
                     this.status = 'killed';
                 }
                 else
@@ -148,13 +148,11 @@ export class GameShip {
                 hitReport = this.status;
                 return hitReport;
             }
-            
+
         }
-        
+
         return hitReport;
     }
-
-
 }
 
 export class Game {
@@ -216,7 +214,7 @@ export function updateTurn(gameID: number | string) {
     }
 }
 
-export function attackFeedback(attackFeedback:attackReport, gameID: number | string) {
+export function attackFeedback(attackFeedback: attackReport, gameID: number | string) {
     const currentGame: RunningGame | undefined = runningGames.get(gameID);
     if (currentGame) {
         rooms[currentGame.roomID].roomUsers.forEach(player => {
@@ -227,7 +225,43 @@ export function attackFeedback(attackFeedback:attackReport, gameID: number | str
             }
         });
     }
-    
+
+}
+
+export function surroundingCellsWipe(currentGame: RunningGame, attackerID: number | string, killCell: types.coordinate) {
+    let attackerIndex = currentGame.players.indexOf(attackerID);
+    let victimIndex = attackerIndex ? 0 : 1;
+
+    let deadShipID;
+    for (let i = 0; i < currentGame.ships[victimIndex].length; i++) {
+        let ship = currentGame.ships[victimIndex][i];
+        if (ship.status == 'killed') {
+            for (let n = 0; n < ship.cells.length; n++) {
+                if (ship.cells[n].x == killCell.x && ship.cells[n].y == killCell.y) {
+                    deadShipID = i;
+                }
+            }
+        }
+    }
+    if (currentGame.ships[victimIndex][deadShipID]) {
+        for (let i = 0; i < currentGame.ships[victimIndex][deadShipID].cells.length; i++) {
+            const currentShipCell = currentGame.ships[victimIndex][deadShipID].cells[i];
+            for (let wx = -1; wx <= 1; wx++)
+                for (let wy = -1; wy <= 1; wy++) {
+                    const wipedCell: types.coordinate = {
+                        x: currentShipCell.x + wx,
+                        y: currentShipCell.y + wy
+                    }
+                    if (currentGame.isValidShot(wipedCell, attackerID)) {
+                        currentGame.damagedCells[victimIndex].push(wipedCell);
+                        console.log(`wiping coord ${wipedCell}`)
+                        attackFeedback(currentGame.attack(wipedCell, attackerID), currentGame.gameID);
+                    }
+                }
+
+        }
+    }
+    else console.log(`it broke\n ${currentGame.ships[victimIndex]} \n ${deadShipID} \n `)
 }
 
 
@@ -237,25 +271,28 @@ export function attack(data) {
     if (currentGame && currentGame.players[currentGame.turn] == data.indexPlayer && currentGame.isValidShot(attackedCell, data.indexPlayer)) {
         const attackReport = currentGame.attack(attackedCell, data.indexPlayer);
         attackFeedback(attackReport, data.gameId);
-
-        if (attackReport.status == 'miss')
-        {
+        if (attackReport.status == 'killed') {
+            surroundingCellsWipe(currentGame, data.indexPlayer, attackedCell);
+        }
+        if (attackReport.status == 'miss') {
             currentGame.turn = 1 - currentGame.turn;
             updateTurn(currentGame.gameID)
         }
     }
 }
-export function random_attack(data) {
 
+
+export function random_attack(data) {
     const currentGame: RunningGame | undefined = runningGames.get(data.gameId);
-    
-    if (currentGame && currentGame.players[currentGame.turn] == data.indexPlayer) {4
+    if (currentGame && currentGame.players[currentGame.turn] == data.indexPlayer) {
+        4
         const attackedCell: types.coordinate = currentGame.getRandomCoordinate(data.indexPlayer);
         const attackReport = currentGame.attack(attackedCell, data.indexPlayer);
         attackFeedback(attackReport, data.gameId);
-
-        if (attackReport.status == 'miss')
-        {
+        if (attackReport.status == 'killed') {
+            surroundingCellsWipe(currentGame, data.indexPlayer, attackedCell);
+        }
+        if (attackReport.status == 'miss') {
             currentGame.turn = 1 - currentGame.turn;
             updateTurn(currentGame.gameID)
         }
@@ -263,8 +300,8 @@ export function random_attack(data) {
 }
 function coordinateExists(arr: types.coordinate[], coord: types.coordinate): boolean {
     return arr.some(c => c.x === coord.x && c.y === coord.y);
-  }
-  
+}
+
 
 export const requestHandler = (req: types.reqInputInt, socket: WebSocket) => {
     let data = (req.data) ? JSON.parse(req.data) : '';
